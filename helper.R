e148db70fc9d93b8b972c039c8088c66811fc3f9
rekkasa <alexandros.rekkas@gmail.com>
2024-10-10 18:49:06 +0300

    add function to target variables in subgroups

--- helper.R
+++ helper.R
@@ -41,3 +41,39 @@ count_within_subgroups <- function(data, subgroup_settings, count_variable) {
     dplyr::group_by(dplyr::across(targeted_subgroups)) |>
     dplyr::summarise(n = sum(get(count_variable)))
 }
+
+count_subgroups_with_percentage <- function(data, subgroup_settings, target_variable) {
+  targeted_subgroups <- rep("", length(subgroup_settings))
+  for (i in seq_along(subgroup_settings)) {
+    if (!is.null(subgroup_settings[[i]]$subgroup_definition)) {
+      data <- data |>
+        subgroup_settings[[i]]$subgroup_definition() |>
+        dplyr::rename("{subgroup_settings[[i]]$subgroup_label}" := "result")
+    }
+    targeted_subgroups[[i]] <- subgroup_settings[[i]]$subgroup_label
+  }
+
+  subgroups_without_target_variable <- targeted_subgroups[-which(targeted_subgroups == target_variable)]
+
+  data |> dplyr::group_by(dplyr::across(subgroups_without_target_variable)) |>
+    tidyr::nest() |>
+    dplyr::mutate(
+      total = purrr::map_dbl(data, nrow),
+    ) |>
+    tidyr::unnest(data) |>
+    dplyr::group_by(
+      dplyr::across(
+        c(subgroups_without_target_variable, target_variable, total)
+      )
+    ) |>
+    tidyr::nest() |>
+    dplyr::mutate(
+      n = purrr::map_dbl(data, nrow),
+      percentage = n / total * 100
+    ) |>
+    dplyr::select(-data) |>
+    dplyr::relocate(total, .after = n) |>
+    dplyr::arrange(dplyr::across(c(subgroups_without_target_variable, target_variable)))
+}
+
+
